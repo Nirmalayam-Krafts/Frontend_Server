9ac3689f3efa4e1af755e2c0269d93ee72b52411
kiran04-code <kr551344@gmail.com>
2026-04-04 18:58:17 +0530

    startWroking on intrigation

---
+++ src/context/Adminauth.jsx
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { createContext ,useContext} from "react"
+
+const AuthContext = createContext()
+
+const axiosInstance  = axios.create({
+ baseURL:import.meta.env.VITE_API_BASE_URL,
+ withCredentials: true,
+})
+export const AuthContextProvider = ({children})=>{
+ return (
+    <AuthContext.Provider value={axiosInstance}>
+        {children}
+    </AuthContext.Provider>
+ )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = ()=>{
+    return useContext(AuthContext)
+}
